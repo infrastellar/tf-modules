0ee546aca50542b3a93b143b41ba875732cb180c
Curt Micol <curt@moonswitch.com>
2023-04-13 13:42:01 -0400

    Remove ns for now

--- modules/form-secret/variables.tf
+++ modules/form-secret/variables.tf
@@ -1,7 +1,3 @@
-variable "namespace" {
-  type = string
-}
-
 variable "secret" {
   type = string
 }
@@ -17,7 +13,6 @@ variable "tags" {
 }
 
 locals {
-  namespec        = "%s/%s"
-  name            = format(local.namespec, lower(var.namespace), lower(var.secret))
+  name            = lower(secret)
   recovery_window = var.tier != "development" ? 30 : 0
 }
